ad140bb4d363f7962ed2ab3fe94a93fd3e3ff7fc
christinaqtruong <christinaqtruong@gmail.com>
2020-05-25 14:51:20 -0500

    Updates

---
+++ precache-manifest.807a0e2f649b025bb4940c37db892af0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a822cd8014e00990d6f812890371d87b",
+    "url": "/reactrecipes/index.html"
+  },
+  {
+    "revision": "b12ae6b2d98ea1933c55",
+    "url": "/reactrecipes/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "49d0ebbcc6497c5cc2a9",
+    "url": "/reactrecipes/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "b12ae6b2d98ea1933c55",
+    "url": "/reactrecipes/static/js/2.9852d9c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactrecipes/static/js/2.9852d9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d0ebbcc6497c5cc2a9",
+    "url": "/reactrecipes/static/js/main.dcda6c7f.chunk.js"
+  },
+  {
+    "revision": "384be5eb511bf75d903b",
+    "url": "/reactrecipes/static/js/runtime-main.46b534da.js"
+  }
+]);
